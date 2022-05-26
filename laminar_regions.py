e89447b6f456794fc03e0d7d3feaa50e9a7894aa
Bryn Jones <bryn.m.f.jones@gmail.com>
2022-05-26 18:40:49 +0100

    Fixed path bifurcation bug, added position data generation

--- laminar_regions.py
+++ laminar_regions.py
@@ -8,6 +8,14 @@ def main(su2_mesh_filepath,root_leading_edge_ID,tip_leading_edge_ID,root_trailin
 
     #Find leading/trailing edges
     leading_edge = a_star_search(root_leading_edge_ID,tip_leading_edge_ID,node_connectivity)
+    print('Found leading edge')
+    trailing_edge = a_star_search(root_trailing_edge_ID,tip_trailing_edge_ID,node_connectivity)
+    print('Found trailing edge')
+
+    #Generate nodal position data
+    position_data = initialise_position_data(su2_mesh_filepath)
+
+
 
     return relative_chord, which_region
 
@@ -67,8 +75,7 @@ def a_star_search(start_node,target_node,node_connectivity):
             if element not in blacklist:
                 new_node_list.append(element)
 
-        if target_node not in node_list:
-            node_score = breadth_search(new_node_list,node_connectivity,node_score,blacklist,target_node)
+        node_score = breadth_search(new_node_list,node_connectivity,node_score,blacklist,target_node)
 
         return node_score
 
@@ -76,18 +83,39 @@ def a_star_search(start_node,target_node,node_connectivity):
 
     current_score = node_score[target_node]
 
-    def get_optimal_path(current_score,current_node,node_connectivity,node_score,optimal_path):
+    def get_optimal_path(current_score,current_node,node_connectivity,node_score,optimal_path,exit_flag=False):
 
         for element in node_connectivity[current_node]:
-            if node_score[element] == 0:
-                return [element]
-            elif node_score[element] == current_score-1:
-                optimal_path = get_optimal_path(current_score-1,element,node_connectivity,node_score,optimal_path)
-                optimal_path.append(element)
+            if not exit_flag:
+                if node_score[element] == 0:
+                    return [element], True
+                elif node_score[element] == current_score-1:
+                    optimal_path, exit_flag = get_optimal_path(current_score-1,element,node_connectivity,node_score,optimal_path,exit_flag)
+                    if exit_flag:
+                        optimal_path.append(element)
 
-        return optimal_path
+        return optimal_path, True
 
-    optimal_path = get_optimal_path(current_score,target_node,node_connectivity,node_score,[target_node])
+    optimal_path = get_optimal_path(current_score,target_node,node_connectivity,node_score,[target_node])[0]
     optimal_path.append(target_node)
 
     return optimal_path
+
+def initialise_position_data(su2_mesh_filepath):
+
+        mesh_file = open(su2_mesh_filepath,'r')
+        mesh_data = mesh_file.read()
+        mesh_file.close()
+
+        mesh_data = re.split('NPOIN= ',mesh_data)
+        mesh_data = re.split('\n',mesh_data[1])
+        number_of_elements = int(mesh_data[0])
+
+        #Parse "edge list", i.e. list of elements by node
+        pos_list = np.zeros((number_of_elements,3))
+        for a in range(1,number_of_elements+1):
+            current_element = re.split('\t',mesh_data[a])
+            for b in range(3):
+                pos_list[a-1,b] = current_element[b]
+
+        return pos_list
