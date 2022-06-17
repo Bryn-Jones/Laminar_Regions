27ad8004e987fc3f6737aab7d98dee4a9b29aaab
Bryn Jones <bryn.m.f.jones@gmail.com>
2022-06-17 12:47:01 +0100

    Created a linear interpolator that enables consistent sigmoid shape

--- laminar_regions.py
+++ laminar_regions.py
@@ -64,13 +64,25 @@ def main(su2_mesh_filepath,root_leading_edge_ID,tip_leading_edge_ID,root_lower_t
         save_list_as_csv(lower_pathlist,'lower_nodes.csv')
         save_list_as_csv(upper_pathlist,'upper_nodes.csv')
 
-    leftovers = np.zeros((max_node_num+1),dtype=int)
+    lower_set = set()
+    upper_set = set()
+
     for row in lower_pathlist:
         for element in row:
-            leftovers[int(element)] = 1
+            lower_set.add(int(element))
+
     for row in upper_pathlist:
         for element in row:
-            leftovers[int(element)] = 1
+            upper_set.add(int(element))
+
+    leftovers = np.zeros((max_node_num+1),dtype=int)
+    for a in range(max_node_num+1):
+        if a in lower_set or a in upper_set:
+            leftovers[a] = 1
+
+    element_tree = generate_element_tree(edge_list,position_data,lower_set,upper_set)
+
+    print('generated element tree')
 
     # print('Lower pathlist lengths')
     # for a in range(len(lower_pathlist)):
@@ -112,10 +124,12 @@ def main(su2_mesh_filepath,root_leading_edge_ID,tip_leading_edge_ID,root_lower_t
 
     correction_spline_list = read_chordwise_corrections('laminar_distribution.csv')
 
+    lower_laminar_spline, upper_laminar_spline, lower_turbulent_spline, upper_turbulent_spline = load_CF_corrections(max_nlf_lower,max_nlf_upper)
+
     print('created correction splines')
 
-    lower_surface = process_laminarity(leading_edge,trailing_edge_lower,su2_data,span_direction,chord_direction,leading_span,max_nlf_lower,banned_span,efficiency,max_node_num,leading_edge_span_length[0],leading_edge_spline,trailing_edge_lower_spline,lower_pathlist,correction_spline_list,correction_efficiencies)
-    upper_surface = process_laminarity(leading_edge,trailing_edge_upper,su2_data,span_direction,chord_direction,leading_span,max_nlf_upper,banned_span,efficiency,max_node_num,leading_edge_span_length[0],leading_edge_spline,trailing_edge_upper_spline,upper_pathlist,correction_spline_list,correction_efficiencies)
+    lower_surface = process_laminarity(leading_edge,trailing_edge_lower,su2_data,span_direction,chord_direction,leading_span,max_nlf_lower,banned_span,efficiency,max_node_num,leading_edge_span_length[0],leading_edge_spline,trailing_edge_lower_spline,lower_pathlist,correction_spline_list,correction_efficiencies,lower_laminar_spline,lower_turbulent_spline,element_tree,edge_list,-1)
+    upper_surface = process_laminarity(leading_edge,trailing_edge_upper,su2_data,span_direction,chord_direction,leading_span,max_nlf_upper,banned_span,efficiency,max_node_num,leading_edge_span_length[0],leading_edge_spline,trailing_edge_upper_spline,upper_pathlist,correction_spline_list,correction_efficiencies,upper_laminar_spline,upper_turbulent_spline,element_tree,edge_list,1)
     leftover_surface = process_leftovers(leftovers,su2_data,max_node_num)
 
     print('generated all laminarity metadata')
@@ -126,6 +140,8 @@ def main(su2_mesh_filepath,root_leading_edge_ID,tip_leading_edge_ID,root_lower_t
     print('wrote data to csv files')
 
     write_vtk_file('wing.vtk',position_data,max_node_num,edge_list,number_of_elements,[lower_surface,upper_surface,leftover_surface])
+
+    print('wrote data to vtk files')
 
     return
 
@@ -286,7 +302,7 @@ def path_spatial_length(path,position_data,direction):
 
     return path_length
 
-def process_laminarity(leading_edge,trailing_edge,su2_data,span_direction,chord_direction,leading_span,max_nlf,banned_span,efficiency,max_node_num,root_span,leading_edge_spline,trailing_edge_spline,node_list,correction_spline_list,correction_efficiencies):
+def process_laminarity(leading_edge,trailing_edge,su2_data,span_direction,chord_direction,leading_span,max_nlf,banned_span,efficiency,max_node_num,root_span,leading_edge_spline,trailing_edge_spline,node_list,correction_spline_list,correction_efficiencies,laminar_spline,turbulent_spline,element_tree,edge_list,lower_or_upper):
 
     output_data = np.zeros((max_node_num,12))
     counter = 0
@@ -314,17 +330,40 @@ def process_laminarity(leading_edge,trailing_edge,su2_data,span_direction,chord_
                     current_banned_span = True
                     break
 
-            if current_efficiency < efficiency and not current_banned_span:
-                laminar_or_not = 1
-
+            if not current_banned_span:
                 corrections_list = []
 
                 for c in range(len(correction_spline_list)):
                     corrections_list.append(correction_spline_list[c](percent_chord))
 
                 efficiency_spline = CubicSpline(correction_efficiencies, corrections_list, axis=0)
-                current_pressure = current_pressure*efficiency_spline(efficiency)
-                current_friction = current_friction*efficiency_spline(efficiency)
+                corrected_pressure = current_pressure*shifted_sigmoid(percent_chord-(max_nlf*efficiency),0.00005) + (1-shifted_sigmoid(percent_chord-(max_nlf*efficiency),0.00005))*current_pressure*efficiency_spline(efficiency)
+                # corrected_friction = current_friction*shifted_sigmoid(percent_chord-(max_nlf*efficiency),0.00005) + (1-shifted_sigmoid(percent_chord-(max_nlf*efficiency),0.00005))*current_friction*efficiency_spline(efficiency)
+            else:
+                corrected_pressure = current_pressure
+                # corrected_friction = current_friction
+
+            percent_transition = max_nlf*efficiency
+
+            if not current_banned_span and percent_chord < percent_transition+0.1 and efficiency > 0.1:
+
+                if percent_chord < percent_transition:
+                    corrected_friction = current_friction * laminar_spline(percent_transition) / turbulent_spline(percent_transition)
+                else:
+                    transition_chord = percent_transition*(trailing_chord-leading_chord) + leading_chord
+                    laminar_transition_cf = interpolate_field_values(su2_data,[15,16,17],element_tree,edge_list,[transition_chord,current_span],True,lower_or_upper) * laminar_spline(percent_transition) / turbulent_spline(percent_transition)
+                    turbulent_transition_cf = interpolate_field_values(su2_data,[15,16,17],element_tree,edge_list,[transition_chord+0.1,current_span],True,lower_or_upper)
+                    corrected_friction = turbulent_transition_cf*shifted_sigmoid(percent_chord-percent_transition,0.1) +  laminar_transition_cf*(1-shifted_sigmoid(percent_chord-percent_transition,0.1))
+                    corrected_friction[corrected_friction>current_friction] = current_friction[corrected_friction>current_friction]
+
+            else:
+                corrected_friction = current_friction
+
+            corrected_pressure = current_pressure*mirrored_shifted_sigmoid(percent_span,0.02,banned_span) + (1-mirrored_shifted_sigmoid(percent_span,0.02,banned_span))*corrected_pressure
+            corrected_friction = current_friction*mirrored_shifted_sigmoid(percent_span,0.02,banned_span) + (1-mirrored_shifted_sigmoid(percent_span,0.02,banned_span))*corrected_friction
+
+            if current_efficiency < efficiency and not current_banned_span:
+                laminar_or_not = 1
             else:
                 laminar_or_not = 0
 
@@ -334,8 +373,8 @@ def process_laminarity(leading_edge,trailing_edge,su2_data,span_direction,chord_
             output_data[counter,5] = percent_chord
             output_data[counter,6] = current_efficiency
             output_data[counter,7] = laminar_or_not
-            output_data[counter,8] = current_pressure
-            output_data[counter,9:12] = current_friction
+            output_data[counter,8] = corrected_pressure
+            output_data[counter,9:12] = corrected_friction
             counter += 1
 
     output_data = output_data[~np.all(output_data == 0, axis=1)]
@@ -480,16 +519,278 @@ def read_chordwise_corrections(the_filepath):
 
     return correction_spline_list
 
+def shifted_sigmoid(x,width):
+
+    #Put x through the sigmoid function, but have x=0 be at the y=0 part of the sigmoid
+    #x must be a normalised value from 0 to 1
+
+    initial_boundary = 3.
+    width_multiplier = 2.*initial_boundary/width
+    scaled_x = x*width_multiplier
+    scaled_x -= initial_boundary
+
+    output = 1. / (1. + np.exp(-scaled_x))
+
+    return output
+
+def mirrored_shifted_sigmoid(x,width,ban_list):
+
+    #Betwixt the ban_list, the output of this function shall tend to 0
+
+    if x > ban_list[-1,1]:
+        x = 1
+
+    for a in range(len(ban_list[:,0])):
+        if x < ban_list[a,0]:
+            ban_ID = a
+            before_ID = True
+            break
+        elif x <= ban_list[a,1]:
+            ban_ID = a
+            before_ID = False
+            break
+
+    if before_ID:
+        if ban_ID == 0:
+            x1 = -x
+        else:
+            x1 = -x+ban_list[ban_ID-1,1]
+
+        x2 = x-ban_list[ban_ID,0]
+        sigmoid_1 = shifted_sigmoid(x1,width)
+        sigmoid_2 = shifted_sigmoid(x2,width)
+        output = (x - ban_list[ban_ID-1,1]) / (ban_list[ban_ID,0]-ban_list[ban_ID-1,1])
+        if ban_ID == 0:
+            output = sigmoid_2
+        else:
+            output = sigmoid_1*(1.-output) + sigmoid_2*output
+
+    else:
+        x1 = x-ban_list[ban_ID,0]
+        x2 = -x+ban_list[ban_ID,1]
+        sigmoid_1 = shifted_sigmoid(x1,width)
+        sigmoid_2 = shifted_sigmoid(x2,width)
+        output = (x - ban_list[ban_ID,0]) / (ban_list[ban_ID,1] - ban_list[ban_ID,0])
+        if ban_ID == len(ban_list[:,0]):
+            output = sigmoid_1
+        else:
+            output = sigmoid_1*(1.-output) + sigmoid_2*output
+
+    return output
+
+def load_CF_corrections(max_nlf_lower,max_nlf_upper):
+
+    laminar_lower = 'Laminar 1.csv'
+    laminar_upper = 'Laminar 2.csv'
+    turbulent_lower = 'Turbulent 1.csv'
+    turbulent_upper = 'Turbulent 2.csv'
+
+    lower_laminar_data = np.genfromtxt(laminar_lower, delimiter=',')
+    upper_laminar_data = np.genfromtxt(laminar_upper, delimiter=',')
+    lower_turbulent_data = np.genfromtxt(turbulent_lower, delimiter=',')
+    upper_turbulent_data = np.genfromtxt(turbulent_upper, delimiter=',')
+
+    # overall_min = 1.e10
+    # overall_max = -1.e10
+    # for element in [lower_laminar_data,upper_laminar_data,lower_turbulent_data,upper_turbulent_data]:
+    #     if np.min(element[:,0]) < overall_min:
+    #         overall_min = np.min(element[:,0])
+    #     if np.max(element[:,0]) > overall_max:
+    #         overall_max = np.max(element[:,0])
+
+    lower_laminar_data[:,0] = lower_laminar_data[:,0] - np.min(lower_laminar_data[:,0])
+    lower_laminar_data[:,0] = lower_laminar_data[:,0] / np.max(lower_laminar_data[:,0])
+    upper_laminar_data[:,0] = upper_laminar_data[:,0] - np.min(upper_laminar_data[:,0])
+    upper_laminar_data[:,0] = upper_laminar_data[:,0] / np.max(upper_laminar_data[:,0])
+    lower_turbulent_data[:,0] = lower_turbulent_data[:,0] - np.min(lower_turbulent_data[:,0])
+    lower_turbulent_data[:,0] = lower_turbulent_data[:,0] / np.max(lower_turbulent_data[:,0])
+    upper_turbulent_data[:,0] = upper_turbulent_data[:,0] - np.min(upper_turbulent_data[:,0])
+    upper_turbulent_data[:,0] = upper_turbulent_data[:,0] / np.max(upper_turbulent_data[:,0])
+
+    for a in range(1,len(lower_laminar_data[:,1])):
+        if lower_laminar_data[a,1] > lower_laminar_data[a-1,1]:
+            transition_point_lower = a
+            transition_lower_value = lower_laminar_data[a,0]
+            break
+
+    lower_laminar_data[:,0] = lower_laminar_data[:,0] / (transition_lower_value / max_nlf_lower)
+    lower_turbulent_data[:,0] = lower_turbulent_data[:,0] / (transition_lower_value / max_nlf_lower)
+
+    for a in range(1,len(upper_laminar_data[:,1])):
+        if upper_laminar_data[a,1] > upper_laminar_data[a-1,1]:
+            transition_point_upper = a
+            transition_upper_value = upper_laminar_data[a,0]
+            break
+
+    upper_laminar_data[:,0] = upper_laminar_data[:,0] / (transition_upper_value / max_nlf_upper)
+    upper_turbulent_data[:,0] = upper_turbulent_data[:,0] / (transition_upper_value / max_nlf_upper)
+
+    lower_laminar_spline = CubicSpline(lower_laminar_data[:,0], lower_laminar_data[:,1], axis=0)
+    upper_laminar_spline = CubicSpline(upper_laminar_data[:,0], upper_laminar_data[:,1], axis=0)
+    lower_turbulent_spline = CubicSpline(lower_turbulent_data[:,0], lower_turbulent_data[:,1], axis=0)
+    upper_turbulent_spline = CubicSpline(upper_turbulent_data[:,0], upper_turbulent_data[:,1], axis=0)
+
+    # print(lower_laminar_data)
+    # print(lower_turbulent_data)
+    # print(transition_point_lower)
+    # for a in range(20):
+    #     print([str(a/(20/max_nlf_lower)),lower_laminar_spline(a/(20/max_nlf_lower))/lower_turbulent_spline(a/(20/max_nlf_lower))])
+    #     #print([str(a/(20/max_nlf_lower)),lower_laminar_spline(a/(20/max_nlf_lower))])
+    # raise ValueError
+
+    return lower_laminar_spline, upper_laminar_spline, lower_turbulent_spline, upper_turbulent_spline
+
+def bool_array_to_decimal(the_array):
+
+    cumsum = 0
+    for a in range(len(the_array)):
+        if the_array[-a-1]:
+            cumsum += 2**a
+
+    return cumsum
+
+def generate_element_tree(edge_list,position_data,lower_set,upper_set):
+
+    element_position_list = np.zeros((len(edge_list[:,0]),3))
+
+    for a in range(len(edge_list[:,0])):
+
+        cumulative_position = np.zeros((3))
+
+        for b in range(len(edge_list[a,:])):
+            cumulative_position += position_data[int(edge_list[a,b]),:]
+
+        element_position_list[a,:] = cumulative_position / (b+1)
+
+    class Point_tree:
+        def __init__(self,the_points,point_list,depth,lower_set,upper_set,edge_list):
+            self.point_list = point_list
+            self.point_data = the_points
+            self.lower_set = lower_set
+            self.upper_set = upper_set
+            self.children = []
+            self.centroid_median = np.median(the_points[point_list,:],axis=0)
+            #self.centroid_mean = np.mean(the_points[point_list,:],axis=0)
+
+            if len(point_list) > 10:
+                child_assignment = np.zeros((len(point_list)),dtype=int)
+                for a in range(len(point_list)):
+                    child_assignment[a] = bool_array_to_decimal(the_points[point_list[a],:] < self.centroid_median)
+
+                assignment_list = [[] for _ in range(8)]
+                for a in range(len(child_assignment)):
+                    assignment_list[child_assignment[a]].append(point_list[a])
+
+                for a in range(8):
+                    self.children.append(Point_tree(the_points,assignment_list[a],depth+1,self.lower_set,self.upper_set,edge_list))
+
+            else:
+
+                self.which_set = []
+                for element in point_list:
+                    cumsum = 0
+                    for node in edge_list[element,:]:
+                        if int(node) in lower_set:
+                            cumsum -= 1
+                        elif int(node) in upper_set:
+                            cumsum += 1
+
+                    if cumsum > 0:
+                        self.which_set.append(1)
+                    elif cumsum < 0:
+                        self.which_set.append(-1)
+                    else:
+                        self.which_set.append(0)
+
+        def get_nearest_element(self,the_position,ignore_z,which_set):
+
+            if len(self.children) > 0:
+
+                search_truth_array = the_position < self.centroid_median
+                nearest_element, nearest_distance = self.children[bool_array_to_decimal(search_truth_array)].get_nearest_element(the_position,ignore_z,which_set)
+
+                if ignore_z:
+                    search_truth_array[2] = not search_truth_array[2]
+                    nearest_element_z, nearest_distance_z = self.children[bool_array_to_decimal(search_truth_array)].get_nearest_element(the_position,ignore_z,which_set)
+                    if nearest_distance_z < nearest_distance:
+                        nearest_element = nearest_element_z.copy()
+                        nearest_distance = nearest_distance_z.copy()
+
+            else:
+
+                current_min = 1e10
+                nearest_element = None
+                for a in range(len(self.point_list)):
+                    if which_set == 0 or self.which_set[a] == which_set:
+                        if ignore_z:
+                            current_dist = np.linalg.norm(the_position[0:2] - self.point_data[self.point_list[a],0:2])
+                        else:
+                            current_dist = np.linalg.norm(the_position - self.point_data[self.point_list[a],:])
+                        if current_dist < current_min:
+                            current_min = current_dist
+                            nearest_element = self.point_list[a]
+
+                nearest_distance = current_min
+
+            return nearest_element, nearest_distance
+
+    element_tree = Point_tree(element_position_list,np.arange(0,len(element_position_list[:,0])),0,lower_set,upper_set,edge_list)
+
+    return element_tree
+
+def interpolate_field_values(su2_data,which_fields,element_tree,edge_list,the_point,correct_Z,which_set):
+
+    corrected_point = np.zeros((3))
+
+    if len(the_point) == 2:
+        extended_point = np.zeros((3))
+        extended_point[0:2] = the_point.copy()
+        extended_point[2] = 0.
+    else:
+        extended_point = the_point.copy()
+
+    closest_element, _ = element_tree.get_nearest_element(extended_point,correct_Z,which_set)
+
+
+    if correct_Z:
+        for element in edge_list[closest_element,:]:
+            corrected_point[2] += su2_data[int(element),3]
+
+        corrected_point[2] = corrected_point[2] / len(edge_list[closest_element,:])
+
+    else:
+        corrected_point = the_point.copy()
+
+    distances = []
+
+    for element in edge_list[closest_element,:]:
+        distances.append(np.linalg.norm(corrected_point-su2_data[int(element),1:4]))
+
+    distances = distances/np.sum(distances)
+
+    interpolated_field_values = np.zeros((len(which_fields)))
+
+    for a in range(len(which_fields)):
+        cumsum = 0.
+        for b in range(len(edge_list[closest_element,:])):
+            cumsum += su2_data[int(edge_list[closest_element,a]),which_fields[a]]*distances[b]
+        interpolated_field_values[a] = cumsum
+
+    return interpolated_field_values
+
 banned_span = np.zeros((4,2))
 banned_span[0,:] = [0.,0.03]
 banned_span[1,:] = [0.2,0.25]
-banned_span[2,:] = [0.455,0.48]
+banned_span[2,:] = [0.45,0.48]
 banned_span[3,:] = [0.96,1]
 
 correction_efficiencies = [0,1]
 
-efficiency = 0.
+efficiency = 1.
+
+span_direction = [0,1,0]
+chord_direction = [1,0,0]
 
 su2_filename = 'example_su2_input.csv'
 
-main('mrsbw-V-BASE-newBL.su2',1,136218,4,136222,22500,158565,'wing',[0,1,0],[1,0,0],0.6321,0.6739,efficiency,banned_span,True,True,correction_efficiencies,su2_filename)
+main('mrsbw-V-BASE-newBL.su2',1,136218,4,136222,22500,158565,'wing',span_direction,chord_direction,0.6321,0.6739,efficiency,banned_span,True,True,correction_efficiencies,su2_filename)
